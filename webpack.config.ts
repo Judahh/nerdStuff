110d84f81fbaf09ca1594a6c538f17ab28e1c82e
Judah <judah@MacBook-Pro-de-Judah.local>
2018-10-15 15:53:35 -0300

    update

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,16 +5,20 @@ import * as CompressionPlugin from 'compression-webpack-plugin';
 import * as ExtractTextPlugin from 'extract-text-webpack-plugin';
 require('dotenv').config();
 
+if (process.env.WEBPACK_PRODUCTION === undefined) {
+    process.env.WEBPACK_PRODUCTION = '0';
+}
+
 const config: webpack.Configuration = {
     entry: './app/code/imports/imports.js',
-    output:{
+    output: {
         libraryTarget: 'commonjs',
         path: path.resolve(__dirname, 'dist'),
         filename: 'app.js',
         publicPath: '/dist/'
     },
-    resolve:{
-        extensions:[
+    resolve: {
+        extensions: [
             '.webpack.js',
             '.webpack.ts',
             '.web.js',
@@ -25,11 +29,11 @@ const config: webpack.Configuration = {
         ]
     },
     module: {
-        rules:[
+        rules: [
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
-                include:[
+                include: [
                     path.resolve(__dirname, 'node_modules'),
                     path.resolve(__dirname, 'app/style'),
                     path.resolve(__dirname, 'app/style/fonts')
@@ -44,19 +48,23 @@ const config: webpack.Configuration = {
     plugins: []
 };
 
-if(JSON.parse(process.env.WEBPACK_PRODUCTION)){
+if (config.plugins === undefined) {
+    config.plugins = [];
+}
+
+if (JSON.parse(process.env.WEBPACK_PRODUCTION)) {
     console.log('Production');
     config.plugins.push(
         new UglifyJSPlugin({
-            parallel:true
+            parallel: true
         })
     );
     config.plugins.push(
         new CompressionPlugin({
-            minRatio:1
+            minRatio: 1
         })
     );
-}else{
+} else {
     console.log('Development');
     config.devtool = 'inline-source-map';
     config.plugins.push(
